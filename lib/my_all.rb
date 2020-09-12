ee56f1316391d870120cc5c8e24acb1d03f80dfa
Pbsmith82 <pbsmith82@gmail.com>
2020-09-12 18:10:27 -0400

    my_all

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
 def my_all?(collection)
+ i_counter = 0
+ block_return_values = []
+ while i_counter < collection.length
+    block_return_values << yield(collection[i_counter])
+    i_counter += 1
+ end
 
+ if block_return_values.include?(false)
+    puts false
+    false
+  else
+    puts true
+    true
+  end
 end
+
+my_all?([1,2,3]) {|i| i < 2}
